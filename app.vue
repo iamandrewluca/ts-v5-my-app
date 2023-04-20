25c3112a95a532117416d82ab534d8d19a7ce97a
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2023-04-21 01:29:31 +0300

    init

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ firstItem }}
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Query {
+  contents?: {
+    data: string[]
+  }
+}
+
+let query: Query | undefined = {}
+let firstItem = query?.contents?.data.at(0)
+</script>
